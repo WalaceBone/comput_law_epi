d450fdd2e413e40167e7b2d81aed5ad95aaea621
charlie <charlie@localhost.localdomain>
2020-10-09 17:43:28 +0200

    + Add home page and page not found

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -21,7 +21,7 @@ import {ChartsModule} from 'ng2-charts';
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
--- web/src/app/modules/auth/auth-routing.module.ts
+++ web/src/app/modules/auth/auth-routing.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
-
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
 })
-export class AuthRoutingModule { }
+export class AuthRoutingModule {}
