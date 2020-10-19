02c5abfd0d9a356f9753cc6f85a0cb49fcf7ba1c
charlie <charlie@localhost.localdomain>
2020-10-19 15:39:27 +0200

    + Add basic test for british nationality test

---
+++ server/src/law/interface/law.interface.ts
@@ -0,0 +1,29 @@
+import { ApiProperty } from "@nestjs/swagger";
+
+export class lawForm {
+
+    @ApiProperty({
+        description: "True if is born in uk or a eligible territory false otherwise"
+    })
+    bornBritishTerritory: boolean;
+
+    @ApiProperty({
+        description: "True if on of two parent have the british nationality false otherwise"
+    })
+    isParentBritishNationality: boolean;
+
+    @ApiProperty({
+        description: "True if on of two parent live in uk or eligible territory false otherwise"
+    })
+    isParentLiveBritishTerritory: boolean;
+
+    @ApiProperty({
+        description: "True if on of two parent are member of the armed forces during born"
+    })
+    isParentMemberArmedForces: boolean;
+
+    @ApiProperty({
+        description: "Date of born"
+    })
+    birthDate: string;
+}
--- server/src/law/law.controller.ts
+++ server/src/law/law.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UseGuards, Req, Body } from '@nestjs/common';
+import { ApiTags, ApiBearerAuth, ApiUnauthorizedResponse, ApiOkResponse } from '@nestjs/swagger';
+import { LawService } from './law.service';
+import ErrorDto from 'src/dto/error.dto';
+import { MessageResponseDto } from 'src/dto/messageReponse.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { lawForm } from './interface/law.interface';
 
+@ApiTags('law')
 @Controller('law')
-export class LawController {}
+export class LawController {
+
+    constructor(private readonly lawService: LawService) {}
+
+    @Post('/britishCitizenTest')
+    @ApiBearerAuth()
+    @ApiUnauthorizedResponse({ type: ErrorDto })
+    @ApiOkResponse({ type: MessageResponseDto })
+    @UseGuards(AuthGuard('jwt'))
+    async isABritishCitizen(@Req() request, @Body() data: lawForm) {
+        if (data.bornBritishTerritory === false) {
+            return {message: "You can't have the british nationality if you are not born in uk or eligible territory."};
+        }
+        if (data.isParentBritishNationality === true || data.isParentLiveBritishTerritory === true) {
+            return {message: "Congrats you can have the british nationality"};
+        }
+        if (data.isParentMemberArmedForces === true) {
+            return {message: "Congrats you can have the british nationality"};
+        }
+        return {message: "You can't have the british nationality if you are not born in uk or eligible territory."};
+    }
+}
