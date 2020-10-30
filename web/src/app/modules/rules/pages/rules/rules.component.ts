e8e01151948b28ebaca0b900d8ef0563534f2551
charlie <charlie@localhost.localdomain>
2020-10-30 16:36:10 +0100

    + Add link to go to the real link law

--- web/src/app/modules/rules/pages/rules/rules.component.ts
+++ web/src/app/modules/rules/pages/rules/rules.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { ApiService } from 'src/app/core/api/api.service';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-rules',
@@ -10,7 +11,7 @@ export class RulesComponent implements OnInit {
 
   public reply: any;
 
-  constructor(private api: ApiService) { }
+  constructor(private api: ApiService, @Inject(DOCUMENT) private document: Document) { }
 
   async ngOnInit(): Promise<void> {
     const resp = await this.api.getRules().subscribe(
@@ -20,6 +21,10 @@ export class RulesComponent implements OnInit {
         this.reply = error;
       }
     );
+  }
+
+  goToUrl(): void {
+    this.document.location.href = 'https://www.legislation.gov.uk/ukpga/1981/61#reference-c20369871';
   }
 
 }
